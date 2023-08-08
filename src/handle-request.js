73c8cca2842d0fbebf5ad45d123e484a6f88906d
Bittu <bittukumarthakur@users.noreply.github.com>
2023-08-08 18:32:23 +0530

    refactor: extract html files from src to resources

--- src/handle-request.js
+++ src/handle-request.js
@@ -1,26 +1,25 @@
 const { readFileSync } = require("node:fs");
 
 const respondHomePage = (request, response) => {
-  const body = readFileSync("./src/home-page.html", "utf-8");
+  const body = readFileSync("./resources/home-page.html", "utf-8");
   response.setBody(body);
   response.setStatusCode(200);
   response.send();
 };
 
 const respondAbeliophyllumPage = (request, response) => {
-  const body = readFileSync("./src/abeliophyllum.html", "utf-8");
+  const body = readFileSync("./resources/abeliophyllum.html", "utf-8");
   response.setBody(body);
   response.setStatusCode(200);
   response.send();
 };
 
 const respondAgeratumPage = (request, response) => {
-  const body = readFileSync("./src/ageratum.html", "utf-8");
+  const body = readFileSync("./resources/ageratum.html", "utf-8");
   response.setBody(body);
   response.setStatusCode(200);
   response.send();
 };
-
 
 const handleRequest = (request, response) => {
   const { requestLine } = request;
@@ -45,4 +44,4 @@ const handleRequest = (request, response) => {
 
 module.exports = {
   handleRequest
-}
+};
